53f91506e8c19c92d18658f0106966326ef3e731
Ole Kallerud <ole@kallerud.no>
2024-06-01 16:56:37 +0200

    Update index.ts

--- supabase/functions/notion-fetch/index.ts
+++ supabase/functions/notion-fetch/index.ts
@@ -1,7 +1,3 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
 import { LogType, NotionIndex, NotionLog, SyncAction } from './models.ts'
 import NotionAPI from './notion-api.ts'
 import { NotionItem } from './notion-parser.ts'
