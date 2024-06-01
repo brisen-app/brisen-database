043aa27d727cbd277254b50f0a1c771eca21654a
Ole Kallerud <ole@kallerud.no>
2024-06-01 16:41:05 +0200

    Refactor index.ts

--- supabase/functions/notion-fetch/index.ts
+++ supabase/functions/notion-fetch/index.ts
@@ -2,71 +2,47 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { LogType, NotionLog, SyncAction } from './models.ts'
+import { LogType, NotionIndex, NotionLog, SyncAction } from './models.ts'
 import NotionAPI from './notion-api.ts'
+import { NotionItem } from './notion-parser.ts'
 import { extractRelations, getRelationTable } from './relation-handler.ts'
 import Supabase from './supabase-api.ts'
 
 // TODO: Setup cron https://youtu.be/-U6DJcjVvGo?si=NLUtt5fftG65RcwF
 
+const responseInit: ResponseInit = {
+  status: 200,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
+const logResponse: NotionLog = {
+  title: 'Sync completed',
+  timestamp: new Date(),
+  exact_time: Date.now(),
+  type: LogType.INFO,
+}
+
 Deno.serve(async () => {
   const relations: object[] = []
-
-  const responseInit: ResponseInit = {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }
-  const logResponse: NotionLog = {
-    title: 'Sync completed',
-    timestamp: new Date(),
-    exact_time: Date.now(),
-    type: LogType.INFO,
-  }
 
   try {
     const tables = await NotionAPI.fetchDatabaseIndex()
     const lastSync = await NotionAPI.fetchLastSyncDate()
 
-    // Handle items
     for (const table of tables) {
       console.log('fetching', table.name)
       const items = await NotionAPI.fetchItems(table.id, lastSync)
       console.log('fetched', items.length, table.name)
 
       for (const item of items) {
-        try {
-          switch (item._sync_action) {
-            case SyncAction.PUBLISH:
-              relations.push(...extractRelations(item))
-              await Supabase.pushItem(table.name, item)
-              break
-            case SyncAction.UNPUBLISH:
-              await Supabase.deleteItem(table.name, item)
-              break
-            default:
-          }
-        } catch (error) {
-          if (!(error instanceof Error)) throw error
-          console.warn(error.message)
-          NotionAPI.logError(`Performing '${item._sync_action}' on '${item.id}' failed`, error, item)
-        }
+        const itemRelations = await handleItem(table, item)
+        relations.push(...itemRelations)
       }
     }
 
-    // Handle relations
     for (const relation of relations) {
-      try {
-        const table = getRelationTable(relation)
-        if (!table) continue
-
-        await Supabase.pushItem(table, relation)
-      } catch (error) {
-        if (!(error instanceof Error)) throw error
-        console.warn(error.message)
-        NotionAPI.logWarning(`Pushing relation failed`, error, relation)
-      }
+      await handleRelations(relation)
     }
   } catch (error) {
     console.error(error)
@@ -80,3 +56,37 @@ Deno.serve(async () => {
   NotionAPI.log(logResponse)
   return new Response(JSON.stringify(logResponse), responseInit)
 })
+
+async function handleItem(table: NotionIndex, item: NotionItem) {
+  const relations: object[] = []
+  try {
+    switch (item._sync_action) {
+      case SyncAction.PUBLISH:
+        relations.push(...extractRelations(item))
+        await Supabase.pushItem(table.name, item)
+        break
+      case SyncAction.UNPUBLISH:
+        await Supabase.deleteItem(table.name, item)
+        break
+      default:
+    }
+  } catch (error) {
+    if (!(error instanceof Error)) throw error
+    console.warn(error.message)
+    NotionAPI.logError(`Performing '${item._sync_action}' on '${item.id}' failed`, error, item)
+  }
+  return relations
+}
+
+async function handleRelations(relation: object) {
+  try {
+    const table = getRelationTable(relation)
+    if (!table) return
+
+    await Supabase.pushItem(table, relation)
+  } catch (error) {
+    if (!(error instanceof Error)) throw error
+    console.warn(error.message)
+    NotionAPI.logWarning(`Pushing relation failed`, error, relation)
+  }
+}
