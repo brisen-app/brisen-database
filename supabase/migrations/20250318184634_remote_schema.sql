3cf2c0f7ad246f1e96b6ec2441e462d12a36f36d
Ole Kallerud <ole@kallerud.no>
2025-03-18 20:09:31 +0100

    Fix error

--- supabase/migrations/20250318184634_remote_schema.sql
+++ supabase/migrations/20250318184634_remote_schema.sql
@@ -2,7 +2,7 @@ alter type "public"."card_order" rename to "card_order__old_version_to_be_droppe
 
 create type "public"."card_order" as enum ('starting', 'next', 'ending');
 
-alter table "public"."cards" alter column order type "public"."card_order" using order::text::"public"."card_order";
+alter table "public"."cards" alter column "order" type "public"."card_order" using "order"::text::"public"."card_order";
 
 drop type "public"."card_order__old_version_to_be_dropped";
 
